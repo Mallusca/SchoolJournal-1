5007f107b5ff44aca206e907df2dbbc41a2a37e8
Your Name <gorosdotos@mail.ru>
2019-12-18 18:23:49 +0300

    Added Environment

---
+++ SchoolJournal.Core/AppEnvironment.cs
@@ -0,0 +1,11 @@
+﻿namespace SchoolJournal
+{
+    public static class AppEnvironment
+    {
+        public const string DEVELOMPENT_ENV = "DEVELOPMENT";
+
+        public const string PRODUCTION_ENV = "PRODUCTION";
+
+        public const string ENV_VARIABLE_NAME = "ASPNET_ENV";
+    }
+}
--- SchoolJournal/Global.asax.cs
+++ SchoolJournal/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿namespace SchoolJournal
 {
     using SchoolJournal.App_Start;
+    using System;
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
@@ -9,6 +10,8 @@
     {
         protected void Application_Start()
         {
+            Environment.SetEnvironmentVariable(AppEnvironment.ENV_VARIABLE_NAME, AppEnvironment.DEVELOMPENT_ENV);
+
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
